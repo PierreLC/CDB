ca1cc5d7bade7a9ceb36e37974d479767fc644e6
PierreLC <plecourtois@excilys.com>
2020-02-18 15:26:44 +0100

    Mapper OK

--- src/main/java/mapper/CompanyMapper.java
+++ src/main/java/mapper/CompanyMapper.java
@@ -8,18 +8,16 @@ import model.Company;
 public class CompanyMapper {
 
 	public static Company getCompanyResultSet(ResultSet resDetailCompany) throws SQLException {
-
-		Company company = null;
 		
-		if (resDetailCompany.next()) {
-			long companyId = (resDetailCompany.getLong("company.id"));
-			String companyName = (resDetailCompany.getString("computer.name"));
-
-		 company = new Company.CompanyBuilder().id(companyId).name(companyName)
-					.build();
+		Company company = null;
+	
+		try {
+			long companyID = resDetailCompany.getLong("company.id");
+			String companyName = resDetailCompany.getString("company.name");
+			company = new Company.CompanyBuilder().id(companyID).name(companyName).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-
 		return company;
-
 	}
 }
